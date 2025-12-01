e53fd6ae7dd6895bde184d0277f90e6ad10d119b
KippTheKippster <vaskostoulas@gmail.com>
2025-12-02 00:02:48 +0100

    Add debug-text-area and orientation listener

--- script.js
+++ script.js
@@ -1,24 +1,68 @@
 const socket = new WebSocket('https://thomasina-childly-garnett.ngrok-free.dev/'); 
 
+let textArea = null
+
 console.log("Loading...")
 
 // Event listener for connection open
 socket.addEventListener('open', (event) => {
-    console.log('Connected to WebSocket server!');
-    socket.send('Hello from Web client!');
-});
-
-// Event listener for receiving messages
-socket.addEventListener('message', (event) => {
-    console.log('Message from server: ', event.data);
+    log('Connected to WebSocket server!');
 });
 
 // Event listener for WebSocket close
 socket.addEventListener('close', (event) => {
-    console.log('WebSocket connection closed:', event);
+    log('WebSocket connection closed:', event);
 });
 
 // Event listener for error
 socket.addEventListener('error', (error) => {
-    console.log('WebSocket error:', error);
+    log('WebSocket error:', error);
 });
+
+// Gyro
+
+function onOrientation(data) {
+    log(data)
+}
+
+// --Gyroscope--
+var gyro = {}
+
+//https://trekhleb.dev/blog/2021/gyro-web/
+gyro.addOrientationListener = function(listener) {
+    if (typeof DeviceMotionEvent.requestPermission === 'function') {
+        // Handle iOS 13+ devices.
+        DeviceMotionEvent.requestPermission()
+            .then((state) => {
+                if (state === 'granted') {
+                    window.addEventListener('devicemotion', listener);
+                } else {
+                    console.error('Request to access the orientation was rejected');
+                }
+            })
+            .catch(console.error);
+    } else {
+        // Handle regular non iOS 13+ devices.
+        window.addEventListener('devicemotion', listener);
+    }
+}
+
+
+function log(...data) {
+    if (textArea != null) {
+        data.forEach(element => {
+            textArea.value += element + "\n"
+        }); 
+
+        textArea.scrollTop = textArea.scrollHeight;
+    }
+
+    console.log(data)
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    textArea = document.getElementById("debug-text-area")
+    textArea.value = ""
+
+    gyro.addOrientationListener(onOrientation)
+})
