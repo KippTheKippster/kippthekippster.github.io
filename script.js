aaa8afde0f77d87fa44edf2cf3b9ed162104ba8a
KippTheKippster <43861443+KippTheKippster@users.noreply.github.com>
2025-12-01 23:11:22 +0100

    Add Client Files

---
+++ script.js
@@ -0,0 +1,24 @@
+const socket = new WebSocket('ws://localhost:8080'); 
+
+console.log("Loading...")
+
+// Event listener for connection open
+socket.addEventListener('open', (event) => {
+    console.log('Connected to WebSocket server!');
+    socket.send('Hello from Web client!');
+});
+
+// Event listener for receiving messages
+socket.addEventListener('message', (event) => {
+    console.log('Message from server: ', event.data);
+});
+
+// Event listener for WebSocket close
+socket.addEventListener('close', (event) => {
+    console.log('WebSocket connection closed:', event);
+});
+
+// Event listener for error
+socket.addEventListener('error', (error) => {
+    console.log('WebSocket error:', error);
+});
